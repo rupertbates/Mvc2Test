a665ff88ac6e360b3b0827ba5d1f26380700f5ec
rupertbates <rupert.bates@guardian.co.uk>
2010-03-26 18:59:47 +0000

    First commit

---
+++ MVC2Test.Tests/Domain/TestContent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC2Test.Domain;
+
+namespace MVC2Test.Tests.Domain
+{
+	[TestClass]
+	public class TestContent
+	{
+		[TestMethod]
+		public void Test_can_create_content()
+		{
+			using (var ctx = new ContentModelContainer())
+			{
+				var content = new Content
+				{
+					Title = "My first article",
+					StandFirst = "My test standfirst",
+                    Body = "Ooh yeah love that sexy body"
+				};
+                var author = new Author
+                {
+                    FirstName = "Brian",
+                    Surname = "Clough"
+                };
+                content.Author = author;
+				ctx.Content.AddObject(content);
+                ctx.SaveChanges();
+				Assert.AreNotEqual(0, content.Id);
+                if (ctx.Authors.Count() == 0)
+                    Assert.Fail("Author was not inserted");
+                Assert.AreEqual("Brian", content.Author.FirstName);
+
+                content.Title = "A different title";
+                ctx.SaveChanges();
+                content = ctx.Content.Where(c => c.Title == "A different title").Single();
+
+			}
+		}
+	}
+}
